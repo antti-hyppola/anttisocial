64f5fbbc4dab22b96aaf740622c923794714d5e9
4nt1d0t3 <59311133+4nt1d0t3@users.noreply.github.com>
2020-08-11 09:07:16 +0100

    React front end
    Fully built react front end for anttisocial app.

---
+++ build/precache-manifest.6dd1a705d24524e9b894e12f1c0c451d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e6fe2fff6a4a7d59201dca905b2279",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d23a7499cb018ae823ed",
+    "url": "/static/css/main.e1081b49.chunk.css"
+  },
+  {
+    "revision": "e9afc9e31f59b8b60ff1",
+    "url": "/static/js/2.94d1520d.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.94d1520d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d23a7499cb018ae823ed",
+    "url": "/static/js/main.f1439738.chunk.js"
+  },
+  {
+    "revision": "10454ef27e06e2c2c312",
+    "url": "/static/js/runtime-main.e0528dc9.js"
+  },
+  {
+    "revision": "921e6266365084c7b696a05d9f192b5a",
+    "url": "/static/media/distancing.921e6266.png"
+  },
+  {
+    "revision": "5a021ab95b4d2a0c8e7511c40ca7bb2f",
+    "url": "/static/media/no-img.5a021ab9.png"
+  }
+]);
